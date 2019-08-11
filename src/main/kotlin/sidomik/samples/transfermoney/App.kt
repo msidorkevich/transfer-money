7aa154605f1dbc2f1fcb372f73fd0527fde10740
Mikhail Sidorkevich <michael.sidorkevich@gmail.com>
2019-08-11 23:52:34 +0300

    fixing code formatting

--- src/main/kotlin/sidomik/samples/transfermoney/App.kt
+++ src/main/kotlin/sidomik/samples/transfermoney/App.kt
@@ -1,7 +1,8 @@
 package sidomik.samples.transfermoney
 
 import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.post
 import org.slf4j.LoggerFactory
 import sidomik.samples.transfermoney.logic.MoneyTransferer
 import sidomik.samples.transfermoney.model.Account
@@ -16,30 +17,31 @@ const val ACCOUNTS_FIND_ENDPOINT = "$ACCOUNTS_ENDPOINT/:id"
 
 private val logger = LoggerFactory.getLogger("rest-controller")
 
-val restServer: Javalin = Javalin.create().apply {
-    exception(Exception::class.java) { e, ctx ->
-        logger.error("Error while executing request ${ctx.req}", e)
-        ctx.status(404)
-        ctx.json(e.message ?: "General Error")
+val restServer: Javalin =
+    Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx ->
+            logger.error("Error while executing request ${ctx.req}", e)
+            ctx.status(404)
+            ctx.json(e.message ?: "General Error")
+        }
+    }.routes {
+        post(ACCOUNTS_CREATE_ENDPOINT) { ctx ->
+            val account = ctx.body<Account>()
+            val createdAccount = AccountStorage.create(account)
+            ctx.json(createdAccount)
+            ctx.status(201)
+        }
+        get(ACCOUNTS_FIND_ENDPOINT) { ctx ->
+            val id = ctx.pathParam("id").toLong()
+            ctx.json(AccountStorage.find(id))
+            ctx.status(200)
+        }
+        post(TRANSFERS_ENDPOINT) { ctx ->
+            val transfer = ctx.body<Transfer>()
+            MoneyTransferer.transferMoney(transfer.from, transfer.to, transfer.currency, transfer.amount)
+            ctx.status(200)
+        }
     }
-}.routes {
-    ApiBuilder.post(ACCOUNTS_CREATE_ENDPOINT) { ctx ->
-        val account = ctx.body<Account>()
-        val createdAccount = AccountStorage.create(account)
-        ctx.json(createdAccount)
-        ctx.status(201)
-    }
-    ApiBuilder.get(ACCOUNTS_FIND_ENDPOINT) { ctx ->
-        val id = ctx.pathParam("id").toLong()
-        ctx.json(AccountStorage.find(id))
-        ctx.status(200)
-    }
-    ApiBuilder.post(TRANSFERS_ENDPOINT) { ctx ->
-        val transfer = ctx.body<Transfer>()
-        MoneyTransferer.transferMoney(transfer.from, transfer.to, transfer.currency, transfer.amount)
-        ctx.status(200)
-    }
-}
 
 fun main() {
     restServer.start(7000)
